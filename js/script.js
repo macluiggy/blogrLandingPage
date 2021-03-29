0d3b90f084bf55b22bf3270dbde5204d0635129a
Luiggy Macias <sosoteluiggy@gmail.com>
2021-03-29 16:53:11 -0500

    intentando hacer toogle con el navbar

--- js/script.js
+++ js/script.js
@@ -3,15 +3,24 @@
     var toogleItem = document.getElementsByClassName('toogleItem');
     console.log(toogleItem.length);
     
-    for (let i = 0; i < toogleItem.length; i++) {
+    for (let i = 0; i < toogleItem.length; i++) {//itera por cada elemento
         const item = toogleItem[i];
         item.addEventListener('mouseover', function () {
-            item.innerHTML = item.innerHTML.replace('△', '▽')
+            item.innerHTML = item.innerHTML.replace('△', '▽');//reemplaza el caracter cuando se pase el mouse por el elemento
         })
         item.addEventListener('mouseout', function () {
-            item.innerHTML = item.innerHTML.replace('▽', '△')
+            item.innerHTML = item.innerHTML.replace('▽', '△');//reemplaza el caracter cuando se pase el mouse por el elemento
         })
     }
-
+ //aqui vamos a mostrar y ocultar el contenido con la
+    window.onload = function () {
+        var containerBurger = document.getElementById('containerBurguer');
+    var navBar = document.getElementById('toogleBar');
+    console.log(navBar.length);
+    containerBurger.addEventListener('click',showAndhide, false)
+    function showAndhide() {
+        navBar.classList.toggle("show");
+    }
+    }
     //Fin de la parte del header
-}())
+}());
